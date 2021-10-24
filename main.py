1abc16d774b51e2e21f71fbb9cc4773537b983bc
Shiva Modugu <77855067+ShivaModugu@users.noreply.github.com>
2021-10-24 16:20:47 -0400

    Calculator

--- main.py
+++ main.py
@@ -1,68 +1,8 @@
-# content of calculator.py#"""
+""" content of calculator.py#"""
 
-#def inc(x_value):
-#    """ Increment Function adds one to the x_value"""
-
- #   return x_value + 1
-#def test_answer():
- #   """This Tests the function"""
-  #  assert inc(4) == 5
-
-
-# Program make a simple calculator
-
-# This function adds two numbers
-def add(x, y):
-    return x + y
-
-
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-            break
-
-    else:
-        print("Invalid Input")
+def inc(x_value):
+    """ Increment Function adds one to the x_value"""
+    return x_value + 1
+def test_answer():
+    """This Tests the function"""
+    assert inc(4) == 5
