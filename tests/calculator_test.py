0a3fd7ad252f19c98e3983402c38cbd2c83b6198
Shiva Modugu <77855067+ShivaModugu@users.noreply.github.com>
2021-10-26 21:36:04 -0400

    Divide & Multiply Edited

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -25,8 +25,25 @@ def test_calculator_subtract():
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+
+def test_calculator_get_result():
+    """Testing the Get result method of the calculator"""
+    calc = Calculator()
+    assert calc.get_result() == 0
+
 def test_calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
     result  = calc.multiply_numbers(1,2)
+    assert result == 2
+
+def test_calculator_get_result():
+    """Testing the Get result method of the calculator"""
+    calc = Calculator()
+    assert calc.get_result() == 0
+
+def test_calculator_divide():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result  = calc.divide_numbers(2,1)
     assert result == 2
