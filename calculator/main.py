b55e2a0b4f87d883e9201f55e03f7a8e891cc32e
Shiva Modugu <77855067+ShivaModugu@users.noreply.github.com>
2021-10-28 12:52:44 -0400

    Divide & Multiply Edited

--- calculator/main.py
+++ calculator/main.py
@@ -21,5 +21,9 @@ class Calculator:
         return self.result
     def divide_numbers(self, value_a, value_b):
         """ multiply two numbers and store the result"""
-        self.result = value_a / value_b
-        return self.result
+        try:
+            self.result = value_a / value_b
+        except ZeroDivisionError as err:
+            raise ValueError from err
+        else:
+            return self.result
